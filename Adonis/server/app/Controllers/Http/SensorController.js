7d60c2ae57d59b657472f75daefa2ab5fbd1d3a1
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-07-16 02:26:20 -0500

    MongoDB sensor register.

--- Adonis/server/app/Controllers/Http/SensorController.js
+++ Adonis/server/app/Controllers/Http/SensorController.js
@@ -1,20 +1,50 @@
 'use strict'
 
 const Sensor = use('App/Models/Sensor')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/raspberry1', {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+const SensorMongoSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  sensor_id: Schema.Types.Number,
+  lecturas_dht: Schema.Types.Array,
+  promedio_lecturas: Schema.Types.Array
+});
+
+const SensorMongo = mongoose.model('sensores',SensorMongoSchema)
 
 class SensorController {
 
-    async create({request}){
+    async create({request,response}){
         const { nombre, ubicacion } = await request.all();
-        const sensor = new Sensor();
-        sensor.fill({
-            nombre,
-            ubicacion,
-           
-        });
 
-        await sensor.save()
-        return sensor;
+        const sensor = await Sensor.create({
+          nombre:nombre,
+          ubicacion:ubicacion
+        })
+
+      const sensor_mongo = await this.createSensorMongo(sensor.id)
+
+      return response.status(200).json({
+        sensor:sensor,
+        sensor_mongo:sensor_mongo
+      })
+
+    }
+
+    async createSensorMongo(sensor_id){
+
+      const sensorMongo = new SensorMongo({
+        sensor_id: sensor_id,
+        _id: new mongoose.Types.ObjectId()
+      })
+
+      await sensorMongo.save()
+
+      return sensorMongo
+
     }
 
     async destroy({params}){
@@ -35,8 +65,8 @@ class SensorController {
     async show({params}){
         const {id} = params;
         const sensor = await Sensor.find(id);
-       
-        
+
+
         return sensor;
     }
 
