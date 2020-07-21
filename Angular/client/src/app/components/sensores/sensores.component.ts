7e8f000c4de5595207a91b3a69c5e26de31f2844
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-07-21 18:09:54 -0500

    Lectures fix 2.

--- Angular/client/src/app/components/sensores/sensores.component.ts
+++ Angular/client/src/app/components/sensores/sensores.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { SensoresService, Sensor } from 'src/app/services/sensores.service';
+import { SensoresService } from 'src/app/services/sensores.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import {Sensor} from "../../interfaces/sensor";
 
 @Component({
   selector: 'app-sensores',
@@ -10,30 +11,30 @@ import { NgForm } from '@angular/forms';
 })
 export class SensoresComponent implements OnInit {
 
-  
+
   inboundClick = false;
 
   datos: Sensor = {
     id: null,
     nombre: "",
     ubicacion: "",
-    
+
     editMode: false
 }
 
   sensoresList : Sensor[]
-  
+
 
   constructor(private sensores: SensoresService, private router: Router) { }
 
   ngOnInit(): void {
     this.getSensores()
-  
+
   }
 
-  
 
-  
+
+
 
 
 
@@ -42,33 +43,33 @@ export class SensoresComponent implements OnInit {
     this.sensores.create(this.datos).subscribe(() =>
 
     this.getSensores(),
-        
+
       err => {
           console.error(err)
       }
-      
+
     )
-  
-  } 
+
+  }
 
     update(){
 
     this.sensores.edit(this.datos).subscribe(() =>
 
     this.getSensores(),
-        
+
       err => {
           console.error(err)
       }
-      
+
     )
 
     document.getElementById("btnSubmit").removeAttribute("disabled")
 
 
-  } 
-    
-    
+  }
+
+
   getSensores(): void{
     this.sensores.getSensores().subscribe(sensoresList => ( this.sensoresList = sensoresList))
 
@@ -76,6 +77,7 @@ export class SensoresComponent implements OnInit {
 
     delete(datos: Sensor): void {
       this.sensoresList = this.sensoresList.filter(h => h !== datos)
+      // @ts-ignore
       this.sensores.delete(datos.id).subscribe(() => console.log('datos borrados'))
       console.log(datos.id)
     }
@@ -88,9 +90,9 @@ export class SensoresComponent implements OnInit {
     }
 
     resetForm(form: NgForm){
-           
+
       form.resetForm(); // or form.reset();
-      
+
     }
 
     makeVisible():void{
